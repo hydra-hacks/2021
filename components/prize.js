27008a9d643c44b973fbaf9322b4aedd66fc426f
Ella <72365100+eilla1@users.noreply.github.com>
2021-05-05 01:35:17 -0700

    refactor: prize icons (#17)

--- components/prize.js
+++ components/prize.js
@@ -1,7 +1,7 @@
 export default function Prize({ icon, track, description }) {
   return (
     <div>
-      <img src={icon} alt={track} className="w-8 h-8" />
+      <img src={icon} alt={track} className="w-14 h-14" />
       <h3 className="text-2xl font-poppins font-semibold pb-2">{track}</h3>
       <p className="text-base">{description}</p>
     </div>
