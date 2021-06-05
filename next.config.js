240cff626cb52ad2faeb6cda50c67ca68d717a70
Ella <72365100+eilla1@users.noreply.github.com>
2021-06-05 16:32:11 -0700

    add zoom redirect (#31)

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/zoom',
+        destination: 'https://zoom.us/j/97414330810?pwd=M0xpTytXL1lubHljTWtVSTg2TFcwUT09',
+        permanent: true,
+      },
+    ]
+  },
+}
